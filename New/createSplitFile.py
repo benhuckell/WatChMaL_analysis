0768332a58f37e0f1e239802b4390e10f1d6ab0b
Ben Huckell <b.huckell72@gmail.com>
2021-03-21 11:11:25 -0700

    new

--- New/createSplitFile.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-n_events = 2868354
-
-val_idxs = np.arange(0, int(n_events*0.4))
-test_idxs = np.arange(int(n_events*0.4), int(n_events*0.5))
-train_idxs = np.arange(int(n_events*0.5), n_events)
-np.savez("/fast_scratch/WatChMaL/data/IWCD_fulltank_300_segmentation_idx.npz", val_idxs=val_idxs, test_idxs=test_idxs, train_idxs=train_idxs)
